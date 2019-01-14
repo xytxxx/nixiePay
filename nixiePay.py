1251bd414ea3b1d280f1b7566e9b2d2b809c1464
Yitong Xiao <y84xiao@uwaterloo.ca>
2019-01-14 18:07:10 -0500

    initial commit

---
+++ nixiePay.py
@@ -0,0 +1,42 @@
+import codecs
+import json
+import glob
+import copy
+import sys
+import re
+import os
+import xlwings as xw
+
+# Configurable Variables
+videoTitleCol = 'A'
+videoTitleRow = 3
+
+translatorNameAlipayCol = 'E'
+translatorAlipayRow = 3
+
+timelinerNameCol = 'I'
+timelinerRow = 12
+
+proofReaderNameAlipayCol = 'E'
+proofReaderAlipayRow = 3
+
+# dependent variables
+proofReaderPayAlipayCol = chr(ord(proofReaderNameAlipayCol)+1)
+proofReaderNamePaypalCol = chr(ord(proofReaderNameAlipayCol)+2)
+proofReaderPaypalRow = proofReaderAlipayRow 
+proofReaderPayPaypalCol = chr(ord(proofReaderNameAlipayCol)+3)
+vidoeDurationCol = chr(ord(videoTitleCol)+1)
+translatorPayAlipayCol = chr(ord(translatorNameAlipayCol)+1)
+translatorNamePaypalCol = chr(ord(translatorNameAlipayCol)+2)
+translatorPaypalRow = translatorAlipayRow 
+translatorPayPaypalCol = chr(ord(translatorNameAlipayCol)+3)
+timelinerPayCol = chr(ord(timelinerNameCol)+1)
+
+
+
+
+
+
+
+
+
